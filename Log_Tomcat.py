9d27fdef557da43c6a6c8b26f82a816b918dffba
yaposebastien <yaposebastien@gmail.com>
2017-11-19 08:45:38 -0500

    Definition d'une function afficher une instance log

--- Log_Tomcat.py
+++ Log_Tomcat.py
@@ -9,9 +9,15 @@ class LogTomcat:
 		self.dateLog = dateLog
 		self.contextLog = contextLog
 
+	def affichageLogs(self):
+		return '{} {} {} '.format(self.addressLog, self.dateLog, self.contextLog)
 
 logOne = LogTomcat('10.10.10.1','2017','wwww.python.com')
 logTwo = LogTomcat('10.10.10.100','2015','wwww.abidjan.net')
 
-print(logOne.addressLog, logOne.dateLog, logOne.contextLog)
-print(logTwo.addressLog, logTwo.dateLog, logTwo.contextLog)
+#print(logOne.addressLog, logOne.dateLog, logOne.contextLog)
+#print(logTwo.addressLog, logTwo.dateLog, logTwo.contextLog)
+
+#Utilisation de la function affichageLogs pour l'affichage des instances de Logs
+print(logOne.affichageLogs())
+print(logTwo.affichageLogs())
