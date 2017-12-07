84428dc3d4eda9a684b21cc9cdab9b223583974d
yaposebastien <yaposebastien@gmail.com>
2017-12-07 10:22:21 -0500

    Exception added

--- CISCO/pibonet.py
+++ CISCO/pibonet.py
@@ -9,11 +9,38 @@ les functions concernees par ce dualisme de version sont absolute, divison, prin
 import netmiko
 import json
 
+#Listing Cisco devices
+
+devices = """
+192.168.10.3
+192.168.10.4
+""".strip().splitlines()
+
+#Creditentials
+device_type='cisco_ios'
+username='pi'
+password='!PiNetw@rk2017!'
+
+#List of functions 
+
+def menuOfActions():
+    print('Please Make A Selection To Proceed : \n'
+            +'\t Press [1] To Display the time'
+            )
+    
 
 print('Beginning of Pi Bot network')
 
-#Creation de la connection
-connection = netmiko.ConnectHandler(ip='192.168.10.2', device_type='cisco_ios', 
-                                    username='pi', password='!PiNetw@rk2017!')
-print(connection.send_command('show clock'))
-connection.disconnect()
+#Adding Exception handling for any issues with the connection or other reasons
+
+menuOfActions()
+    
+#Calling the function and loop to display the time
+for device in devices:
+    try:   
+        #Creation de la connection
+        connection = netmiko.ConnectHandler(ip=device,device_type=device_type ,username=username, password=password)
+        print(connection.send_command('show clock'))
+        connection.disconnect()
+    except Exception as e:
+        print('ERROR SYSTEM : ' +e.message)
